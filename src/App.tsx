26df24987fa33d11a70b72427036cd4a04be6edc
Bruno Malagoli <bmalagoli2@gmail.com>
2022-10-17 23:33:39 -0300

    MainPage lazy import and CircularProgress as fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import "./App.css";
 import DataContextProvider from "./contexts/DataContext/DataContextProvider";
-import MainPage from "./pages/MainPage";
-
+import { lazy, Suspense } from "react";
+import { Stack, CircularProgress } from "@mui/material";
+const MainPage = lazy(() => import("./pages/MainPage"));
 function App() {
   return (
     <>
       <DataContextProvider>
-        <MainPage />
+        <Suspense
+          fallback={
+            <Stack
+              width={"100%"}
+              height={"100vh"}
+              alignItems={"center"}
+              justifyContent={"center"}
+            >
+              <CircularProgress />
+            </Stack>
+          }
+        >
+          <MainPage />
+        </Suspense>
       </DataContextProvider>
     </>
   );
