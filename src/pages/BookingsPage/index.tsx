b58b5d98ebdaee7ae4222e7f03a57f0e51965e1e
Bruno Malagoli <bmalagoli2@gmail.com>
2022-10-25 01:44:49 -0300

    AddBookingForm modal implementation and fields validations using formik && yup

--- src/pages/BookingsPage/index.tsx
+++ src/pages/BookingsPage/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useContext } from "react";
 import getBookings from "../../services/Bookings";
 import DataContext from "../../contexts/DataContext/DataContext";
 import BookingFilters from "./components/BookingFilters";
+import NewBookingsForm from "./components/NewBookingsForm";
 
 const BookingsPage = () => {
   const { setAllBookingsData } = useContext(DataContext);
@@ -15,6 +16,7 @@ const BookingsPage = () => {
   return (
     <>
       <NavBar />
+      <NewBookingsForm />
       <BookingFilters />
       <BookingsTable />
     </>
