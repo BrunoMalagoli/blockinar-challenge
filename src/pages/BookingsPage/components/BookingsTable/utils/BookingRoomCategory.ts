38934d9daec2fc8d321508e38f104a43986aed13
Bruno Malagoli <bmalagoli2@gmail.com>
2022-10-26 13:29:10 -0300

    Fixing null being assigned as confort category

--- src/pages/BookingsPage/components/BookingsTable/utils/BookingRoomCategory.ts
+++ src/pages/BookingsPage/components/BookingsTable/utils/BookingRoomCategory.ts
@@ -1,23 +1,27 @@
 export default function getRoomCategory(bookingItemRoomId: number) {
   let category;
-  switch (true) {
-    case bookingItemRoomId < 4:
-      category = "Confort";
-      break;
-    case bookingItemRoomId > 4 && bookingItemRoomId < 7:
-      category = "Superior";
-      break;
-    case bookingItemRoomId > 7 && bookingItemRoomId < 9:
-      category = "Junior Suite";
-      break;
-    case bookingItemRoomId > 9:
-      category = "Senior Suite";
-      break;
-    case bookingItemRoomId === null:
-      category = "Room not assigned";
-    default:
-      category = "Room not assigned";
-      break;
+  if (bookingItemRoomId === null) {
+    category = "Room not assigned";
+  } else {
+    switch (true) {
+      case bookingItemRoomId === null:
+        category = "Room not assigned";
+      case bookingItemRoomId < 4:
+        category = "Confort";
+        break;
+      case bookingItemRoomId > 4 && bookingItemRoomId < 7:
+        category = "Superior";
+        break;
+      case bookingItemRoomId > 7 && bookingItemRoomId < 9:
+        category = "Junior Suite";
+        break;
+      case bookingItemRoomId > 9:
+        category = "Senior Suite";
+        break;
+      default:
+        category = "Room not assigned";
+        break;
+    }
   }
   return category;
 }
